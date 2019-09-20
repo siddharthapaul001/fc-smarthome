97a40fe8b5ccab7e40007f32a31f23b28fc83648
Siddhartha Paul <siddharthapaul@live.com>
2019-09-20 22:41:35 +0530

    express added

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+var express = require('express');
+const app = express();
 
+var commonHeaders = {
+    'Content-Type': 'application/json'
+}
+
+app.get('/', (req, res)=>{
+    res.writeHead(200, {
+        ...commonHeaders
+    })
+
+    res.write(JSON.stringify({
+        'status': 200
+    }))
+
+    res.end();
+});
+
+
+app.listen( process.env.PORT || 5000, () => console.log('running...'));

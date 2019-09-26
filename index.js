ae79f4a3c86a64ace2ee078814d633753de6395c
Siddhartha Paul <siddharthapaul@live.com>
2019-09-26 11:52:07 +0530

    Initiated passport and session

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const passport = require('passport');
+const session = require('express-session');
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 const path = require('path');
 const dbController = require('./utils/dbcontroller');
 const secret = require('./secrets/googleoauth');
@@ -9,7 +12,56 @@ var commonHeaders = {
     'Content-Type': 'application/json'
 }
 
+passport.use(new GoogleStrategy({
+    clientID: secret.GOOGLE_CLIENT_ID,
+    clientSecret: secret.GOOGLE_SECRET,
+    callbackURL: "/auth"
+},
+    function (accessToken, refreshToken, profile, done) {
+        return done(null, profile);
+    }
+));
+passport.serializeUser(function (user, cb) {
+    cb(null, user);
+});
+
+passport.deserializeUser(function (obj, cb) {
+    cb(null, obj);
+});
+
+app.use(passport.initialize());
+app.use(session({
+    secret: 'hoyHoyyyyyyH0yyYyyy@y!!!',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+}));
+
 app.use('/', express.static(path.join(__dirname, 'public')));
+
+app.use('/test', (req, res) => {
+    res.end(JSON.stringify(req.session.user));
+})
+
+app.get('/login', passport.authenticate('google', { scope: ['email', 'profile'] }));
+
+app.get('/auth', passport.authenticate('google'), (req, res) => {
+    req.session.user = {
+        googleId: req.user.id,
+        fullName: req.user.displayName,
+        name: req.user.name['givenName'],
+        email: req.user.emails[0].value,
+        dp: req.user.photos[0].value
+    }
+    res.redirect('/test');
+});
+
+app.get('/logout', (req, res) => {
+    req.session.destroy((err) => {
+        res.end('You are successfully logged out');
+        //res.redirect('https://accounts.google.com/logout');
+    });
+})
 
 // app.get('/', (req, res)=>{
 //     res.writeHead(200, {
@@ -22,4 +74,4 @@ app.use('/', express.static(path.join(__dirname, 'public')));
 // });
 
 
-app.listen( process.env.PORT || 5000, () => console.log('running...'));
+app.listen(process.env.PORT || 5000, () => console.log('running...'));
--- secrets/googleoauth.js
+++ secrets/googleoauth.js
@@ -1,2 +1,4 @@
 const GOOGLE_CLIENT_ID = '695750346649-7kmctdjr5rlgsoajk3npi50sovul7rr5.apps.googleusercontent.com',
 GOOGLE_SECRET = 'lDJ-xACChruzbkiTgVx-GPbT';
+
+module.exports={GOOGLE_CLIENT_ID, GOOGLE_SECRET};
